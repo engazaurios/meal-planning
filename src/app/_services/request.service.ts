a8f8e9e64a59055096f1b4338cf2f5bd39eafebb
Cesar Luis <cesarluis13915@gmail.com>
2019-07-01 16:39:22 -0600

    Add RequestService: Http requests layer.

---
+++ src/app/_services/request.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class RequestService {
+  host: string;
+
+  constructor(private http: HttpClient) {
+    this.host = environment.host;
+  }
+
+  get(url: string, options = {}) {
+    return this.http.get(this.host + url, options);
+  }
+
+  post(url: string, data = {}, options = {}) {
+    return this.http.post(this.host + url, data, options);
+  }
+}
