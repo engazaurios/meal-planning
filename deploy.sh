5030c1c7ddf1fe37b8d1941223e9f8c6a99d9baf
Boris Becerra <bbecerra@paypal.com>
2019-09-16 19:42:13 -0600

    Deploy script fixed

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,11 @@ fi
 # Installs packages
 npm install
 
+echo "Starting to build production environment."
 # Execute build to prod
-ng build --prod true --progress true --verbose true
+ng build --prod true
 
+echo "Cleaning directories."
 # Creates the directory if doesn't exist.
 ssh engazaurio@67.205.147.22 'mkdir -p /var/www/trouw-nutrition.com/html/'
 # Removes backup folder and creates it again.
@@ -29,5 +31,6 @@ ssh engazaurio@67.205.147.22 'rm -rf ~/backup/meal-planning && mkdir -p ~/backup
 # Moves all the content to backup
 ssh engazaurio@67.205.147.22 'mv /var/www/trouw-nutrition.com/html/* ~/backup/meal-planning'
 
+echo "Copying files to remote."
 # Copies all the files to specified directory
 scp -r dist/meal-planning/* engazaurio@67.205.147.22:/var/www/trouw-nutrition.com/html
